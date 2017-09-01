f87e0ce4801f3adc17b77fcbd30b07cab811e909
Daniel Voyles <dman977@gmail.com>
2017-09-01 08:50:13 -0600

    Removed '!nightsister-acolyte'. Wrong file!

--- Test.py
+++ Test.py
@@ -1,26 +1,2 @@
 import discord
 from discord.ext import commands
-
-
-if message.content.startswith("!nightsister-acolyte"): 
-        embed=discord.Embed(title="**Nightsister Acolyte**",color=discord.Colour(i will do this), description="Stealth attacker with some enemy debuffing and self healing ability.")
-        embed.set_footer(text="Salporin | SWGoH", icon_url="https://lh3.googleusercontent.com/cWRI4jOO0fq5KP2HOh6V2BoCFhEwI2XghBMrKiamVgFi3YrxdD7lt0_dyIPhBwmpWQ=w300-rw")
-        embed.set_thumbnail(url="https://swgoh.gg/static/img/assets/tex.charui_nightsister_acolyte.png")
-        embed.set_image(url="https://i.pinimg.com/236x/2f/27/c6/2f27c6e1eaa555044f66f3a4132c5ac5--cosplay--cosplay-costume.jpg")
-        embed.add_field(name=":zeta:", value="No Zeta Ability")
-        embed.add_field(name="Stats", value="`Power: 16,120` `Speed: 147` `Health: 17,638` (:gearicong111:, without mods)")
-        embed.add_field(name="Basic Ability - Strike from the Shadows - ?", value="Deal Physical damage to target enemy with a 55% chance to gain Stealth for 2 turns.")
-        embed.add_field(name="Special Ability - Sacrificial Magicks - ?", value="Deal Special damage to target enemy. On a finishing blow, gain 40% Turn Meter and remove all negative status effects from all allies.")
-        embed.add_field(name="Unique Ability - Thrill of Victory - ?", value="Nightsister Acolyte has +15% Critical Chance and recovers 20% of her Max Health when scoring a Critical Hit.")
-        embed.add_field(name="Character Ratings (**NOTE**: The maximum score is 10)", value="""
-Galactic War:
-Arena:
-Raids:
-
-Overall:
-----------------------------------------
-Farmability: <emoji>
-                    """)
-        embed.add_field(name="Usage", value="type usage here")
-        embed.add_field(name="Ahnald Ranking", value="**Youngling**")
-        
